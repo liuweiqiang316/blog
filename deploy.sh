69a87f57002ef902484925f4f3b2ecf0968e4e83
liuweiqiang <liuweiqiang316@163.com>
2022-03-06 22:11:24 +0800

    feat: 调整部署相关

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:liuweiqiang316/blog.git main:blog
+git push -f git@github.com:liuweiqiang316/blog.git master:blog
 
 cd -
