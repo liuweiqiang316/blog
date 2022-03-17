1f61b1218a627114b9f9835759734ac9b3ff032b
liuweiqiang <liuweiqiang316@163.com>
2022-03-17 23:30:49 +0800

    feat: 更新路由信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,74 +1,77 @@
+const meta = require('../meta.json')
+
 module.exports = {
     // 站点配置
     lang: 'zh-CN',
     title: '微斯人的博客',
     description: '微斯人的杂货铺',
-    base:'/blog/',
+    base: '/blog/',
 
     // 主题和它的配置
     theme: '@vuepress/theme-default',
     themeConfig: {
         // logo: 'https://vuejs.org/images/logo.png',
-        sidebar: [
-            {
-                text: 'JavaScript基础',
-                link: '/JavaScript基础'
-            },
-            {
-                text: 'JavaScript异步',
-                link: '/JavaScript异步'
-            },
-            {
-                text: '手写',
-                link: '/手写',
-                children: [
-                    {
-                        text: 'Promise',
-                        link: '/手写/promise.md'
-                    },
-                    {
-                        text: 'isArray',
-                        link: '/手写/isArray.md'
-                    },
-                    {
-                        text: 'reduce',
-                        link: '/手写/reduce.md'
-                    },
-                    {
-                        text: 'sym',
-                        link: '/手写/sym.md'
-                    },
-                    {
-                        text: '生成器执行器',
-                        link: '/手写/run.md'
-                    },
-                ]
-            },
-            {
-                text: 'vue源码',
-                link: '/vue源码'
-            },
-            {
-                text: 'http常用知识',
-                link: '/http常用知识'
-            },
-            {
-                text: 'Linux常用命令',
-                link: '/Linux常用命令'
-            },
-            {
-                text: 'git',
-                link: '/git'
-            },
-            {
-                text: '代码片段',
-                link: '/代码片段'
-            },
-            {
-                text: 'vue.js设计与实现的阅读笔记',
-                link: '/vue.js设计与实现的阅读笔记'
-            },
-        ],
+        sidebar: meta,
+        // sidebar: [
+        //     {
+        //         text: 'JavaScript基础',
+        //         link: '/JavaScript基础'
+        //     },
+        //     {
+        //         text: 'JavaScript异步',
+        //         link: '/JavaScript异步'
+        //     },
+        //     {
+        //         text: '手写',
+        //         link: '/手写',
+        //         children: [
+        //             {
+        //                 text: 'Promise',
+        //                 link: '/手写/promise.md'
+        //             },
+        //             {
+        //                 text: 'isArray',
+        //                 link: '/手写/isArray.md'
+        //             },
+        //             {
+        //                 text: 'reduce',
+        //                 link: '/手写/reduce.md'
+        //             },
+        //             {
+        //                 text: 'sym',
+        //                 link: '/手写/sym.md'
+        //             },
+        //             {
+        //                 text: '生成器执行器',
+        //                 link: '/手写/run.md'
+        //             },
+        //         ]
+        //     },
+        //     {
+        //         text: 'vue源码',
+        //         link: '/vue源码'
+        //     },
+        //     {
+        //         text: 'http常用知识',
+        //         link: '/http常用知识'
+        //     },
+        //     {
+        //         text: 'Linux常用命令',
+        //         link: '/Linux常用命令'
+        //     },
+        //     {
+        //         text: 'git',
+        //         link: '/git'
+        //     },
+        //     {
+        //         text: '代码片段',
+        //         link: '/代码片段'
+        //     },
+        //     {
+        //         text: 'vue.js设计与实现的阅读笔记',
+        //         link: '/vue.js设计与实现的阅读笔记'
+        //     },
+        // ],
         contributors: false,
         lastUpdatedText: '最后更新于',
     },
